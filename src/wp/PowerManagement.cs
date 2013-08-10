b698d5245694900b8342718d3e0ee052a55dddf2
viras <viras@users.sourceforge.net>
2013-08-10 14:21:21 +0200

    New Cordova 3.0 structure for plugin

---
+++ src/wp/PowerManagement.cs
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2013 Wolfgang Koller
+ * 
+ * This file is part of GOFG Sports Computer - http://www.gofg.at/.
+ * 
+ * GOFG Sports Computer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * GOFG Sports Computer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with GOFG Sports Computer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Phone.Shell;
+using WPCordovaClassLib.Cordova;
+using WPCordovaClassLib.Cordova.Commands;
+using WPCordovaClassLib.Cordova.JSON;
+
+namespace Cordova.Extension.Commands
+{
+    class PowerManagement : BaseCommand
+    {
+        public void acquire(string options) {
+            PhoneApplicationService.Current.UserIdleDetectionMode = IdleDetectionMode.Disabled;
+            
+            DispatchCommandResult();
+        }
+
+        public void release(string options)
+        {
+            PhoneApplicationService.Current.UserIdleDetectionMode = IdleDetectionMode.Enabled;
+            
+            DispatchCommandResult();
+        }
+
+    }
+}
